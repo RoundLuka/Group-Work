06d0c3e9667153a7d404a2c2f3a2ac766f28cf96
GalacticBob <bob707029@gmail.com>
2025-05-22 20:05:00 +0400

    commit

---
+++ Banking-Program/main.py
@@ -0,0 +1,9 @@
+
+# მთავარი კოდი - კაპიტანმა
+name = input("Enter your name: ")
+password = input("Enter  your password: ")
+
+data = [name, password]
+
+# გუნდის წვერი - ლუკა ცვლილებები
+print("გამარჯობა კაპიტანო")
